5687815ad32787ede83a46bbba0f128339a0576e
Kyle Winckler <kyle.winckler@gmail.com>
2016-11-26 13:00:12 -0800

    non-blocking requests

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 var http = require('http');
 var url = require('url');
 
-function start(route) {
+function start(route, handle) {
   function onRequest(req, res) {
     var pathname = url.parse(req.url).pathname;
     console.log('Request for ' + pathname + ' received.');
 
-    route(pathname);
-
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World');
-    res.end();
+    route(handle, pathname, res);
   }
 
   http.createServer(onRequest).listen(8888);
